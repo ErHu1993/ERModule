84c5f3e2eecad25ee9424d1880f04b27d2d2f254
huguangyu <199301055@qq.com>
2017-07-03 16:57:51 +0800

    no message

---
+++ ERModuleDemo/ERModuleDemo/Classes/CTMediator+GetServiceViewController.h
@@ -0,0 +1,15 @@
+//
+//  CTMediator+GetServiceViewController.h
+//  Pods
+//
+//  Created by 胡广宇 on 2017/7/3.
+//
+//
+
+#import "CTMediator.h"
+
+@interface CTMediator (GetServiceViewController)
+
+- (UIViewController *)GetServiceViewController_TabBarViewController;
+
+@end
